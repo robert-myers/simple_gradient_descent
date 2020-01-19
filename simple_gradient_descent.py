aaedfb628eea11da0e1cfec40394e40ff1a0b5a5
robert-myers <53881487+robert-myers@users.noreply.github.com>
2020-01-18 21:02:04 -0800

    updated x to X to match common convention and added docstring to gradient_descent function

--- simple_gradient_descent.py
+++ simple_gradient_descent.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def get_gradient_at_b(x, y, b, m):
+def get_gradient_at_b(X, y, b, m):
 	"""
 	Parameters
 	----------
-	x : array_like of int or float
+	X : array_like of int or float
 		The X coordinates.
 	y : array_like of int or float
 		The Y coordinates.
@@ -18,17 +18,17 @@ def get_gradient_at_b(x, y, b, m):
 	b_gradient : float
 		The gradient value at the intercept.
 	"""
-	x, y = np.array(x), np.array(y)
-	N = len(x)
-	diff = np.sum(y - (m * x + b))
+	X, y = np.array(X), np.array(y)
+	N = len(y)
+	diff = np.sum(y - (m * X + b))
 	b_gradient = -2/N * diff
 	return b_gradient
 
-def get_gradient_at_m(x, y, b, m):
+def get_gradient_at_m(X, y, b, m):
 	"""
 	Parameters
 	----------
-	x : array_like of int or float
+	X : array_like of int or float
 		The X coordinates.
 	y : array_like of int or float
 		The Y coordinates.
@@ -42,17 +42,17 @@ def get_gradient_at_m(x, y, b, m):
 	m_gradient : float
 		The gradient value at the slope.
 	"""
-	x, y = np.array(x), np.array(y)
-	N = len(x)
-	diff = np.sum(x * (y - (m * x + b)))
+	X, y = np.array(X), np.array(y)
+	N = len(y)
+	diff = np.sum(X * (y - (m * X + b)))
 	m_gradient = -2/N * diff
 	return m_gradient
 
-def step_gradient(x, y, b_current, m_current, learning_rate=0.01):
+def step_gradient(X, y, b_current, m_current, learning_rate=0.01):
 	"""
 	Parameters
 	----------
-	x : array_like of int or float
+	X : array_like of int or float
 		The X coordinates.
 	y : array_like of int or float
 		The Y coordinates.
@@ -68,18 +68,42 @@ def step_gradient(x, y, b_current, m_current, learning_rate=0.01):
 	step : array of float
 		The new intercept value and the new slope value.
 	"""
-	b_gradient = get_gradient_at_b(x, y, b_current, m_current)
-	m_gradient = get_gradient_at_m(x, y, b_current, m_current)
+	b_gradient = get_gradient_at_b(X, y, b_current, m_current)
+	m_gradient = get_gradient_at_m(X, y, b_current, m_current)
 	b = b_current - (learning_rate * b_gradient)
 	m = m_current - (learning_rate * m_gradient)
 	step = np.array([b, m])
 	return step
 
-def gradient_descent(x, y, learning_rate, num_iterations, b=0, m=0, y_predict=False):
+def gradient_descent(X, y, b=0, m=0, learning_rate=0.01, num_iterations=1000, y_predict=False):
+	"""
+	Parameters
+	----------
+	X : array_like of int or float
+		The X coordinates.
+	y : array_like of int or float
+		The Y coordinates.
+	b : int or float
+		The starting intercept guess (the default is 0).
+	m : int or float
+		The starting intercept guess (the default is 0).
+	learning_rate : float
+		The proportional size of step to take (the default is 0.01).
+	num_iterations : int
+		The number of steps to descend (the default is 1000).
+	y_predict : bool
+		Whether or not to calculate the y_predictions and return them.
+
+	Returns
+	-------
+	b, m, y_predictions : tuple of float and optional array
+		The final intercept value, the final slope value,
+		and optionally the predicted values of y.
+	"""
 	for _ in range(num_iterations):
-		b, m = step_gradient(x, y, b, m, learning_rate)
+		b, m = step_gradient(X, y, b, m, learning_rate)
 	if y_predict:
-		y_predictions = m * x + b
+		y_predictions = m * X + b
 		return b, m, y_predictions
 	else:
 		return b, m
